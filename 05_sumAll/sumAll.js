a12356049f4760b4850605e9318bc13aac94d8d6
freeram <kulikovpavel2801@gmail.com>
2024-01-04 05:17:29 -0700

    Solve 05-06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+    if (!(typeof from === "number") || !(typeof to === "number")) {
+        return "ERROR";
+    } else if (from < 0 || to < 0) {
+        return "ERROR";
+    } else if (from > to) {
+        let temp = from;
+        from = to;
+        to = temp;
+    }
 
+    let sum = 0;
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
