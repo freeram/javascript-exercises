25f95d31b3ff225a1adc10d1873a6e19f104d1f7
freeram <kulikovpavel2801@gmail.com>
2024-01-04 04:33:36 -0700

    Solve 01-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if (count < 0) {
+        return "ERROR"
+    }
 
+    let output = '';
+    for (let i = 0; i < count; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
