b8c3a0edfe8dc0818ff067a9b2036e9da1ff4288
freeram <kulikovpavel2801@gmail.com>
2024-01-04 05:03:04 -0700

    Solve 03-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    let reversed = "";
 
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        reversed += stringArray[i];
+    }
+    return reversed
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    const array = Array.from(arguments[0]);
 
+    let indexToRemove;
+    for (var i = 1; i < arguments.length; i++) {
+        indexToRemove = array.indexOf(arguments[i]);
+        if (indexToRemove == -1) {
+            continue;
+        }
+        array.splice(indexToRemove, 1);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
