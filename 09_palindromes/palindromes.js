68bdedfb773edba96070622bd414ea4623e64c9b
freeram <kulikovpavel2801@gmail.com>
2024-05-14 14:12:55 -0600

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arr = string
+        .split('')
+        .filter(el => el.match(/[a-zA-Z0-9]/i))
+        .map(el => el.toLowerCase());
+    const rev = [...arr].reverse();
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== rev[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
