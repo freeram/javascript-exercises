5eff7d816e41873a95339c179788daf738a0c342
freeram <kulikovpavel2801@gmail.com>
2024-05-14 14:30:34 -0600

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(k) {
+    if (k < 0) {return "OOPS";}
+    else if (k == 0) {return 0;}
 
+    let a = 1, b = 1, c;
+    for (let i = 1; i < k; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
 };
+
+// 1, 1, 2, 3, 5, 8    
 
 // Do not edit below this line
 module.exports = fibonacci;
