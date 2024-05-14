fbae398c28844428373f32bde34662cc8da9e36e
freeram <kulikovpavel2801@gmail.com>
2024-05-14 14:40:09 -0600

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let currentAge = (current.yearOfDeath ?? new Date().getFullYear()) - current.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath ?? new Date().getFullYear()) - oldest.yearOfBirth;
+        return currentAge > oldestAge ? current : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
